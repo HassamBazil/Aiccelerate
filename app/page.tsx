f3543ee77c85ba2a4a5ab7bcf2c53dccc5635ded
hassambazil <hassambazil98@gmail.com>
2025-01-09 18:57:36 +0500

    target blank

--- app/page.tsx
+++ app/page.tsx
@@ -257,6 +257,7 @@ export default function Home() {
           <Link 
             href="https://x.com/aicceleratedao" 
             className="nav-link flex items-center md:pr-8 lg:pr-1"
+            target="_blank"
           >
             <Twitter className="w-5 h-5 sm:w-6 sm:h-6" />
           </Link>
